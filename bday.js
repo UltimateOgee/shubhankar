cb31327ce4bb66a0518a6847b875bcaefc2211fa
BuildTools <unconfigured@null.spigotmc.org>
2021-04-22 16:17:04 -0700

    const shwabble

--- bday.js
+++ bday.js
@@ -1,18 +1,19 @@
 $(function () {
-    var calcNewYear = setInterval(function () {
-        date_bday = new Date(1999, 3, 28, 0, 0, 0, 0);
-        date_now = new Date();
+    'use strict';
+    const calcNewYear = setInterval(function () {
+        const date_bday = new Date(1999, 3, 28, 0, 0, 0, 0);
+        const date_now = new Date();
 
-        seconds = Math.floor((date_now - (date_bday)) / 1000);
-        minutes = Math.floor(seconds / 60);
-        hours = Math.floor(minutes / 60);
-        days = Math.floor(hours / 24);
-        years = Math.floor(days / 365);
+        const seconds = Math.floor((date_now - (date_bday)) / 1000);
+        const minutes = Math.floor(seconds / 60);
+        const hours = Math.floor(minutes / 60);
+        const days = Math.floor(hours / 24);
+        const years = Math.floor(days / 365);
 
-        days = days - (years * 365);
-        hours = hours - (days * 24) - (years * 365 * 24);
-        minutes = minutes - (days * 24 * 60) - (hours * 60) - (years * 365 * 24 * 60);
-        seconds = seconds - (days * 24 * 60 * 60) - (hours * 60 * 60) - (minutes * 60) - (years * 365 * 24 * 60 * 60);
+        const days = days - (years * 365);
+        const hours = hours - (days * 24) - (years * 365 * 24);
+        const minutes = minutes - (days * 24 * 60) - (hours * 60) - (years * 365 * 24 * 60);
+        const seconds = seconds - (days * 24 * 60 * 60) - (hours * 60 * 60) - (minutes * 60) - (years * 365 * 24 * 60 * 60);
 
         $("#time").text("Shubh's age is  " + years + " years, " + days + " days, " + hours + " hours, " + minutes + " minutes, and " + seconds + " seconds");
     }, 1000);
